f7899491a15d005a68514a489e86bf96cbfe6300
Viktor <x.viktor.13@gmail.com>
2020-12-15 20:11:03 +0700

    Added homepage template

--- user/views.py
+++ user/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
 
 
 def main(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect('/home/')
-    else:
-        return render(request, 'user/main.html')
+    return render(request, 'user/main.html')
 
 
 @login_required
